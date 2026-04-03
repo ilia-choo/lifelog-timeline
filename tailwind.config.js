ca441acb2dd12962c01308abbe24e2b4358d1a05
Ilia <ilia@gubgoo.com>
2026-04-03 10:21:18 +0900

    chore: define tailwindcss theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          light: "#60a5fa",
+          DEFAULT: "#3b82f6",
+          dark: "#1d4ed8",
+        },
+        surface: {
+          50: "#f8fafc",
+          100: "#f1f5f9",
+          600: "#475569",
+          900: "#0f172a",
+        },
+      },
+    },
   },
   plugins: [],
 };
