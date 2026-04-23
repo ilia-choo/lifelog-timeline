0810a9045155550b42f2f19035165a4b30fc7766
Ilia <ilia@gubgoo.com>
2026-04-23 14:32:26 +0900

    refactor: reorganize component exports into categorized sub-directories for improved maintainability

---
+++ src/components/layout/index.ts
@@ -0,0 +1,3 @@
+export * from "./site-footer";
+export * from "./site-header";
+export * from "./theme-toggle";
---
+++ src/components/timeline/index.ts
@@ -0,0 +1,4 @@
+export * from "./filter-bar";
+export * from "./milestone-form";
+export * from "./timeline-item";
+export * from "./timeline-list";
